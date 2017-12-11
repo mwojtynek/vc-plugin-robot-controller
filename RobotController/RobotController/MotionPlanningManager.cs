343b8fde2848b9cb3bb21701462f17fa6ef4e1be
Hendrik Oestreich <hoestreich@techfak.uni-bielefeld.de>
2017-12-11 19:08:31 +0100

    First version of multi robot support in robot controller - needs lots of enhancements!

---
+++ RobotController/RobotController/MotionPlanningManager.cs
@@ -0,0 +1,126 @@
+﻿using Caliburn.Micro;
+using System;
+using System.ComponentModel.Composition;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VisualComponents.Create3D;
+using VisualComponents.UX.Shared;
+
+namespace RobotController
+{
+    public class MotionPlanningManager
+    {
+        public Dictionary<IRobot, MotionPlan> MotionPlanners { get; }
+
+        public MotionPlanningManager()
+        {
+            MotionPlanners = new Dictionary<IRobot, MotionPlan>();
+
+        }
+
+        /// <summary>
+        /// Initialize a motionPlanner instance for a specific robot. 
+        /// This instance is stored in the dictonary of the class which manages and maintains the instances of all motionPlanners of all robots.
+        /// </summary>
+        /// <param name="robot"></param>The VC robot instance for which a motionPlanner instance should be created
+        /// <param name="pathToRobotUrdfDescriptionFile"></param>The URDF description of the robot
+        /// <param name="kinChainStart"></param>The name of the first part in the kinematic chain.
+        /// <param name="kinChainEnd"></param>The name of the last part in the kinematic chain.
+        /// <param name="pathToObstacleFile"></param>The path to an obstacle model file (e.g. stl format).
+        public void InitializeMotionPlanner(IRobot robot, String pathToRobotUrdfDescriptionFile, String kinChainStart, String kinChainEnd, String pathToObstacleFile)
+        {
+            if (robot.Component.GetProperty("motionStartTime") == null) { 
+                robot.Component.CreateProperty(typeof(double), PropertyConstraintType.NotSpecified, "motionStartTime");
+            }
+            if (robot.Component.GetProperty("motionEndTime") == null)
+            {
+                robot.Component.CreateProperty(typeof(double), PropertyConstraintType.NotSpecified, "motionEndTime");
+            }
+
+            MotionPlan motionPlan = new MotionPlan();
+            motionPlan.loadMotionPlanRobotDescription(pathToRobotUrdfDescriptionFile, kinChainStart, kinChainEnd);
+            MotionPlanRobotDescription description = motionPlan.getMotionPlanRobotDescription();
+            
+            Vector3 wpr = robot.Component.TransformationInWorld.GetWPR();
+
+            description.setRobotPosition(robot.Component.TransformationInWorld.GetP().X / 1000,
+                                        robot.Component.TransformationInWorld.GetP().Y / 1000,
+                                        robot.Component.TransformationInWorld.GetP().Z / 1000);
+            description.setRobotRotation(wpr.X, wpr.Y, wpr.Z);
+
+            motionPlan.addObstacle(pathToObstacleFile);
+            if (!MotionPlanners.ContainsKey(robot))
+            {
+                MotionPlanners.Add(robot, motionPlan);
+            }
+        }
+
+        /// <summary>
+        /// Add additional obstacles to a motionPlanner instance of the specified robot.
+        /// </summary>
+        /// <param name="robot"></param>The robot for which the obstacle should be relevant.
+        /// <param name="pathToObstacleFile"></param>The path to an obstacle model file (e.g. stl format).
+        public void addObstacleToRobot(IRobot robot, String pathToObstacleFile)
+        {
+            MotionPlanners[robot].addObstacle(pathToObstacleFile);
+        }
+
+
+        /// <summary>
+        /// Trigger and receive a concrete motionPlan from startFrame to goalFrame.
+        /// Obstacles must be added to the motionPlanner instance of the robot before.
+        /// </summary>
+        /// <param name="robot"></param>The robot for which a motion should be planned.
+        /// <param name="startFrame"></param>The frame where the motion should start.
+        /// <param name="goalFrame"></param>The frame where the motion should end.
+        /// <returns></returns>
+        public VectorOfDoubleVector planMotion(IRobot robot, String startFrame, String goalFrame)
+        {
+            MotionPlanRobotDescription description = MotionPlanners[robot].getMotionPlanRobotDescription();
+
+            VectorOfDouble vec = new VectorOfDouble(robot.Controller.Joints.Count);
+            vec.Add(robot.Controller.Joints[0].Value);
+            vec.Add(robot.Controller.Joints[1].Value);
+            vec.Add(robot.Controller.Joints[6].Value);
+            vec.Add(robot.Controller.Joints[2].Value);
+            vec.Add(robot.Controller.Joints[3].Value);
+            vec.Add(robot.Controller.Joints[4].Value);
+            vec.Add(robot.Controller.Joints[5].Value);
+
+            //TODO: Make those frames global?
+            IFeature startNode = robot.Component.FindFeature(startFrame);
+            IFeature goalNode = robot.Component.FindFeature(goalFrame);
+
+            Matrix startPosition = robot.Component.RootNode.GetFeatureTransformationInWorld(startNode);
+            Matrix goalPosition = robot.Component.RootNode.GetFeatureTransformationInWorld(goalNode);
+            Vector3 startRotation = startPosition.GetWPR();
+            Vector3 goalRotation = goalPosition.GetWPR();
+
+            VectorOfDouble startJointAngles = description.getIK(startPosition.GetP().X / 1000,
+                                                                startPosition.GetP().Y / 1000,
+                                                                startPosition.GetP().Z / 1000,
+                                                                startRotation.X, startRotation.Y, startRotation.Z);
+            VectorOfDouble goalJointAngles = description.getIK(goalPosition.GetP().X / 1000,
+                                                                goalPosition.GetP().Y / 1000,
+                                                                goalPosition.GetP().Z / 1000,
+                                                                goalRotation.X, goalRotation.Y, goalRotation.Z);
+            
+            MotionPlanners[robot].setStartPosition(startJointAngles);
+            MotionPlanners[robot].setGoalPosition(goalJointAngles);
+
+            MotionPlanners[robot].setSolveTime(10.0);
+            MotionPlanners[robot].setStateValidityCheckingResolution(0.001);
+            //motionPlan.setReportFirstExactSolution(true);
+            MotionPlanners[robot].setPlannerByString("RRTConnect");
+
+            if (MotionPlanners[robot].plan() >= 0)
+            {
+                //motionPlan.interpolatePath()
+                return MotionPlanners[robot].getLastResult();
+            }
+            return null;
+        }
+    }
+}
--- RobotController/RobotController/RobotController.cs
+++ RobotController/RobotController/RobotController.cs
@@ -16,19 +16,12 @@ namespace RobotController
     public class RobotController : IPlugin
     {
         private IApplication app = null;
-        private static RobotController instance = null;
-        private IRoutine customProgram = null;
         private IMessageService ms = null;
-        private ReaderWriterLockSlim varRdWrLock = new ReaderWriterLockSlim();
-        private ReaderWriterLockSlim jntRdWrLock = new ReaderWriterLockSlim();
-        private VectorOfDoubleVector motionPlanResult = null;
+        private static RobotController instance = null;
         private double timeElapsedBase = 0.0;
-        private MotionPlan motionPlan = null;
-        private IRobot globalRobot = null;
         private IStatisticsManager statisticsManager = null;
         private ISamplingTimer timer = null;
-        private double motionStartTime = 0.0;
-        private double motionEndTime = 0.0;
+        public MotionPlanningManager MotionPlanningManagerInstance { get; }
 
         //Create Constructor for class
         [ImportingConstructor]
@@ -36,10 +29,9 @@ namespace RobotController
         {
             this.app = app;
             instance = this;
-            this.app.Simulation.SimulationStarted += SetInitialTime;
+            MotionPlanningManagerInstance = new MotionPlanningManager();
             this.app.Simulation.SimulationStarted += SimulationStarted;
             this.app.Simulation.SimulationStopped += SimulationStopped;
-            this.app.Simulation.SimulationRun += SimulationRunLogger;
         }
 
         public static RobotController getInstance()
@@ -50,7 +42,6 @@ namespace RobotController
         public void Exit()
         {
         }
-
 
         /*
          * Get StatisticsManager instance, enable it and set an interval!
@@ -86,93 +77,12 @@ namespace RobotController
         {
             ms.AppendMessage("Current Time: " + app.Simulation.Elapsed, MessageLevel.Warning);
 
-            interpolatePlannedMotion(globalRobot);
-        }
-
-        public MotionPlan InitializeMotionPlanner(IRobot robot)
-        {
-            //TODO: Dirty hack!! Remove!
-            globalRobot = robot;
-            robot.RobotController.Heartbeat += HeartbeatLogger;
-            motionPlan = new MotionPlan();
-            motionPlan.loadMotionPlanRobotDescription("S:\\git\\rosi.plugin.pathplanner\\robot_descriptions\\urdf\\lbr_iiwa_14_r820.urdf", "base_link", "tool0");
-            MotionPlanRobotDescription description = motionPlan.getMotionPlanRobotDescription();
-
-
-            Vector3 wpr = robot.Component.TransformationInWorld.GetWPR();
-
-            description.setRobotPosition(robot.Component.TransformationInWorld.GetP().X / 1000,
-                                        robot.Component.TransformationInWorld.GetP().Y / 1000,
-                                        robot.Component.TransformationInWorld.GetP().Z / 1000);
-            description.setRobotRotation(wpr.X, wpr.Y, wpr.Z);
-
-            motionPlan.addObstacle("S:/git/rosi.plugin.pathplanner/cage-models/fleximir-model-even-less-detailed.stl");
-
-            return motionPlan;
-        }
-
-        public VectorOfDoubleVector planMotion(IRobot robot, String startFrame, String goalFrame, MotionPlan motionPlan)
-        {
-            MotionPlanRobotDescription description = motionPlan.getMotionPlanRobotDescription();
-
-            VectorOfDouble vec = new VectorOfDouble(robot.Controller.Joints.Count);
-            vec.Add(robot.Controller.Joints[0].Value);
-            vec.Add(robot.Controller.Joints[1].Value);
-            vec.Add(robot.Controller.Joints[6].Value);
-            vec.Add(robot.Controller.Joints[2].Value);
-            vec.Add(robot.Controller.Joints[3].Value);
-            vec.Add(robot.Controller.Joints[4].Value);
-            vec.Add(robot.Controller.Joints[5].Value);
-
-            IFeature startNode = robot.Component.FindFeature(startFrame);
-            IFeature goalNode = robot.Component.FindFeature(goalFrame);
-
-            Matrix startPosition = robot.Component.RootNode.GetFeatureTransformationInWorld(startNode);
-            Matrix goalPosition = robot.Component.RootNode.GetFeatureTransformationInWorld(goalNode);
-            Vector3 startRotation = startPosition.GetWPR();
-            Vector3 goalRotation = goalPosition.GetWPR();
-
-            VectorOfDouble startJointAngles = description.getIK(startPosition.GetP().X / 1000,
-                                                                startPosition.GetP().Y / 1000,
-                                                                startPosition.GetP().Z / 1000,
-                                                                startRotation.X, startRotation.Y, startRotation.Z);
-            VectorOfDouble goalJointAngles = description.getIK(goalPosition.GetP().X / 1000,
-                                                                goalPosition.GetP().Y / 1000,
-                                                                goalPosition.GetP().Z / 1000,
-                                                                goalRotation.X, goalRotation.Y, goalRotation.Z);
-
-            motionPlan.setStartPosition(startJointAngles);
-            motionPlan.setGoalPosition(goalJointAngles);
-
-            motionPlan.setSolveTime(10.0);
-            motionPlan.setStateValidityCheckingResolution(0.001);
-            //motionPlan.setReportFirstExactSolution(true);
-            motionPlan.setPlannerByString("RRTConnect");
-
-            if (motionPlan.plan() >= 0)
+            foreach (IRobot robot in MotionPlanningManagerInstance.MotionPlanners.Keys)
             {
-                motionPlanResult = motionPlan.getLastResult();
-                motionStartTime = app.Simulation.Elapsed;
-                //motionPlan.interpolatePath()
-                return motionPlan.getLastResult();
+                interpolatePlannedMotion(robot);
             }
-            return null;
         }
-
-        public void SetInitialTime(object sender, EventArgs e)
-        {
-            timeElapsedBase = app.Simulation.Elapsed;
-        }
-
-        public void SimulationRunLogger(object sender, EventArgs e)
-        {
-            ms.AppendMessage("Sender: " + sender.ToString(), MessageLevel.Warning);
-        }
-
-        public void HeartbeatLogger(object sender, HeartbeatEventArgs e)
-        {
-            ms.AppendMessage("Sender: " + sender.ToString() + " Time: " + e.Time, MessageLevel.Warning);
-        }
+        
 
         private double[] kukaSorted(VectorOfDouble jointAngleCollection)
         {
@@ -191,9 +101,10 @@ namespace RobotController
         }
         private void interpolatePlannedMotion (IRobot robot)
         {
-            if(motionPlanResult!= null)
+            
+            if (MotionPlanningManagerInstance.MotionPlanners[robot].getLastResult() != null)
             {
-                double[] startJointAngleCollection = kukaSorted(motionPlanResult.First());
+                double[] startJointAngleCollection = kukaSorted(MotionPlanningManagerInstance.MotionPlanners[robot].getLastResult().First());
 
                 IMotionTarget startMotionTarget = robot.RobotController.CreateTarget();
                 startMotionTarget.CartesianSpeed = 1.0;
@@ -203,9 +114,8 @@ namespace RobotController
                 startMotionTarget.MotionType = MotionType.Joint;
                 startMotionTarget.SetAllJointValues(startJointAngleCollection);
                 startMotionTarget.UseJointValues = true;
-
-
-                double[] goalJointAngleCollection = kukaSorted(motionPlanResult.Last());
+                
+                double[] goalJointAngleCollection = kukaSorted(MotionPlanningManagerInstance.MotionPlanners[robot].getLastResult().Last());
 
                 IMotionTarget goalMotionTarget = robot.RobotController.CreateTarget();
                 goalMotionTarget.CartesianSpeed = 1.0;
@@ -216,152 +126,37 @@ namespace RobotController
                 goalMotionTarget.SetAllJointValues(goalJointAngleCollection);
                 goalMotionTarget.UseJointValues = true;
                 
-                //robot.RobotController.Evaluate();
-
                 IMotionInterpolator motionInterpolator = robot.RobotController.CreateMotionInterpolator();
                 motionInterpolator.AddTarget(startMotionTarget);
                 motionInterpolator.AddTarget(goalMotionTarget);
 
-                ms.AppendMessage("MotionInterpolator " + motionInterpolator.Targets.Count, MessageLevel.Warning);
-                ms.AppendMessage("GetCycleTime at 0: " + motionInterpolator.GetCycleTimeAt(0), MessageLevel.Warning);
-                ms.AppendMessage("GetCycleTime at 1: " + motionInterpolator.GetCycleTimeAt(1), MessageLevel.Warning);
-                ms.AppendMessage("GetCycleTime at " + motionInterpolator.Targets.Count + ": " + motionInterpolator.GetCycleTimeAt(motionInterpolator.Targets.Count-1), MessageLevel.Warning);
+                //ms.AppendMessage("GetCycleTime at " + motionInterpolator.Targets.Count + ": " + motionInterpolator.GetCycleTimeAt(motionInterpolator.Targets.Count-1), MessageLevel.Warning);
 
-                if (motionEndTime == 0.0)
+                if (robot.Component.GetProperty("motionEndTime") != null && robot.Component.GetProperty("motionEndTime").Equals("0.0"))
                 {
-                    motionEndTime = motionInterpolator.GetCycleTimeAt(1);
-                    ms.AppendMessage("motion will end at: " + motionEndTime, MessageLevel.Warning);
+                    robot.Component.SetPropertyValues(Enumerable.Repeat<string>("motionStartTime", 1), Enumerable.Repeat<string>(app.Simulation.Elapsed.ToString(), 1));
+                    robot.Component.SetPropertyValues(Enumerable.Repeat<string>("motionEndTime", 1), Enumerable.Repeat<string>(motionInterpolator.GetCycleTimeAt(1).ToString(), 1));
+                    ms.AppendMessage("Motion for robot " + robot.Name + " will end at: " + motionInterpolator.GetCycleTimeAt(1), MessageLevel.Warning);
                 }
                 
                 IMotionTarget refMotionTarget = robot.RobotController.CreateTarget();
 
-                motionInterpolator.Interpolate(app.Simulation.Elapsed-motionStartTime, ref refMotionTarget);
+                motionInterpolator.Interpolate(app.Simulation.Elapsed- (double)robot.Component.GetProperty("motionStartTime").Value, ref refMotionTarget);
                 
 
                 IMotionTester motionTester = robot.RobotController.GetMotionTester();
                 motionTester.CurrentTarget = refMotionTarget;
 
-                if (app.Simulation.Elapsed > motionEndTime)
+                if (app.Simulation.Elapsed > (double)robot.Component.GetProperty("motionEndTime").Value)
                 {
-                    motionStartTime = 0.0;
-                    motionEndTime = 0.0;
-                    motionPlanResult = null;
+                    
+                    robot.Component.SetPropertyValues(Enumerable.Repeat<string>("motionStartTime", 1), Enumerable.Repeat<string>("0.0", 1));
+                    robot.Component.SetPropertyValues(Enumerable.Repeat<string>("motionEndTime", 1), Enumerable.Repeat<string>("0.0", 1));
+                    MotionPlanningManagerInstance.MotionPlanners[robot].invalidatePlanner();
                 }
 
              }
         }
 
-        public void JointConfigurationChanged(object sender, EventArgs e)
-        {
-
-            ISimulationService simulationService = (ISimulationService)sender;
-            ms.AppendMessage("Simulation Service Loop: " + simulationService.Elapsed, MessageLevel.Warning);
-            try
-            {
-                //ISimComponent robotComponent = (ISimComponent)sender;
-
-                varRdWrLock.EnterWriteLock();
-                if (motionPlanResult != null && motionPlanResult.Count > 0 && globalRobot.RobotController != null)
-                {
-                    //  app.WriteLine("Interpolating for " + (app.Simulation.Elapsed - timeElapsedBase));
-                    double timeNow = app.Simulation.Elapsed - timeElapsedBase;
-
-                    if (!motionPlan.isInterpolationDone(timeNow, 20))
-                    {
-                        VectorOfDouble result = motionPlan.interpolatePath(timeNow, 20.0);
-                        List<double> tmp_jointList = new List<double>();
-                        tmp_jointList.Add(result[0]);
-                        tmp_jointList.Add(result[1]);
-                        tmp_jointList.Add(result[3]);
-                        tmp_jointList.Add(result[4]);
-                        tmp_jointList.Add(result[5]);
-                        tmp_jointList.Add(result[6]);
-                        tmp_jointList.Add(result[2]);
-                        globalRobot.RobotController.SetJointValues(tmp_jointList);
-                        globalRobot.RobotController.InvalidateKinChains();
-                    }
-                }
-            }
-            finally
-            {
-                varRdWrLock.ExitWriteLock();
-            }
-
-            //try
-            //{
-
-            //    jntRdWrLock.EnterWriteLock();
-            //    if (jointReader)
-            //    {
-            //        List<double> tmp_jointList = new List<double>();
-            //        tmp_jointList.Add(jointList[0]);
-            //        tmp_jointList.Add(jointList[1]);
-            //        tmp_jointList.Add(jointList[3]);
-            //        tmp_jointList.Add(jointList[4]);
-            //        tmp_jointList.Add(jointList[5]);
-            //        tmp_jointList.Add(jointList[6]);
-            //        tmp_jointList.Add(jointList[2]);
-            //        robot.RobotController.SetJointValues(tmp_jointList);
-            //        robot.RobotController.SetJointValues(tmp_jointList);
-            //        robot.RobotController.InvalidateKinChains();
-            //        jointReader = false;
-            //        //app.WriteLine("Angles from Update Event");
-            //        //app.WriteLine("Joint [0] Degree: " + jointList[0] + " Radians: " + MathConverter.DegreeToRadian(jointList[0]));
-            //        //app.WriteLine("Joint [1] Degree: " + jointList[1] + " Radians: " + MathConverter.DegreeToRadian(jointList[1]));                        
-            //        //app.WriteLine("Joint [3] Degree: " + jointList[3] + " Radians: " + MathConverter.DegreeToRadian(jointList[3]));                        
-            //        //app.WriteLine("Joint [4] Degree: " + jointList[4] + " Radians: " + MathConverter.DegreeToRadian(jointList[4]));                        
-            //        //app.WriteLine("Joint [5] Degree: " + jointList[5] + " Radians: " + MathConverter.DegreeToRadian(jointList[5]));                        
-            //        //app.WriteLine("Joint [6] Degree: " + jointList[6] + " Radians: " + MathConverter.DegreeToRadian(jointList[6]));                        
-            //        //app.WriteLine("Joint [2] Degree: " + jointList[2] + " Radians: " + MathConverter.DegreeToRadian(jointList[2]));
-            //    }
-            //}
-            //finally
-            //{
-            //    jntRdWrLock.ExitWriteLock();
-            //}
-        }
-
-        [Export(typeof(IActionItem))]
-        public class PlanMotion : ActionItem
-        {
-            [Import]
-            private Lazy<IApplication> app = null;
-
-            IMessageService ms = null;
-
-            public PlanMotion() : base("PlanMotion")
-            {
-                ms = IoC.Get<IMessageService>();
-                ms.AppendMessage("Constructor of PlanMotion Action Item called", MessageLevel.Warning);
-            }
-
-            public override void Execute(PropertyCollection args)
-            {
-                ms.AppendMessage("Executing PlanMotion...", MessageLevel.Warning);
-
-                //TODO: Fix the hard index access or at least print out a message if input was wrong
-                String robotName = (String)args.GetByIndex(0).Value;
-                IRobot robot = app.Value.World.FindComponent(robotName).GetRobot();
-
-                MotionPlan motionPlanInstance = RobotController.getInstance().InitializeMotionPlanner(robot);
-
-                String startFrameName = (String)args.GetByIndex(1).Value;
-
-                String goalFrameName = (String)args.GetByIndex(2).Value;
-
-                VectorOfDoubleVector resultMotion = RobotController.getInstance().planMotion(robot, startFrameName, goalFrameName, motionPlanInstance);
-
-                foreach (VectorOfDouble vector in resultMotion)
-                {
-                    String angles = "";
-                    foreach (double angle in vector)
-                    {
-                        angles = angles + " , " + angle;
-                    }
-                    ms.AppendMessage("Angles: " + angles, MessageLevel.Warning);
-                }
-                ms.AppendMessage("Executed PlanMotion.", MessageLevel.Warning);
-            }
-        }
     }
 }
